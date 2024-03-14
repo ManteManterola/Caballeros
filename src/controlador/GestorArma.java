e02ef2068e5dcef4f7e910a66f7efc770a3b8c4b
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-14 10:27:49 +0100

    Estructura gestorArma

--- src/controlador/GestorArma.java
+++ src/controlador/GestorArma.java
@@ -1,8 +1,39 @@
 package controlador;
 
+import java.util.Scanner;
+
+import vista.Menu;
+
 public class GestorArma {
 	
+	private static Scanner scan = new Scanner(System.in);
 	public static void run() {
+		
+		int select;
+		do {
+			Menu.mostrarMenuAdmin();
+			select=Integer.parseInt(scan.nextLine());
+			switch(select) {
 			
+			case Menu.VISUALIZAR_ARMAS:
+				//visualizarArma()
+				break;
+				
+			case Menu.INSERTAR_ARMAS:
+				//INSERTAR_ARMAS()
+				break;
+			case Menu.BORRAR_ARMAS:
+				//BORRAR_ARMAS()
+				break;
+			case Menu.MODIFICAR_ARMAS:
+				//Menu.MODIFICAR_ARMA()
+				break;
+			case Menu.SALIR:
+				
+				break;
+				
+			default:
+			}
+		}while(select!=0);
 		}
 }
