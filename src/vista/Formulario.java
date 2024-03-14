a3cafd666e644284908b116ed5b3cfc52b9137a8
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-14 10:54:48 +0100

    Insertar arma

--- src/vista/Formulario.java
+++ src/vista/Formulario.java
@@ -1,13 +1,22 @@
 package vista;
 
+import java.util.Scanner;
+
 import modelo.*;
 
 public class Formulario {
-
+	private static Scanner scan = new Scanner(System.in);
+	
 	public static Arma pedirDatosArma() {
 		Arma a = new Arma();
 		
-		return a;
+		System.out.println("Inserte el nombre del arma");
+		a.setTipo(scan.nextLine());
 		
+		System.out.println("Inserte el daño del arma");
+		a.setDaño(Integer.parseInt(scan.nextLine()));
+		
+		return a;
 	}
 }
+
