0d78ca83e6f4513e7179c068d6832e7f21cca8dc
V <149675360+xabivacas@users.noreply.github.com>
2024-03-14 10:41:38 +0100

    Revert "Xabi"

--- src/vista/Formulario.java
+++ src/vista/Formulario.java
@@ -1,13 +1,5 @@
 package vista;
 
-import modelo.*;
-
 public class Formulario {
 
-	public static Arma pedirDatosArma() {
-		Arma a = new Arma();
-		
-		return a;
-		
-	}
 }
--- src/vista/Visor.java
+++ src/vista/Visor.java
@@ -1,10 +1,5 @@
 package vista;
 
-import java.util.*;
-
 public class Visor {
 
-	public <T> void mostrarArray(ArrayList<T> lista) {
-		
-	}
 }
